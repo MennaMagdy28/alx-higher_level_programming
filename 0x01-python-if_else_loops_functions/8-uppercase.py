881db272046ec60cde1cb4f5afd972d0d7f790c6
menna magdy <menna11hassanien@gmail.com>
2024-01-03 21:10:13 +0200

    moroX

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in str:
-        if int(a) > 96 and int(a) < 123:
+        if ord(a) > 96 and ord(a) < 123:
             cnv = 32
         else:
             cnv = 0
-        print("{:c}".format(a - cnv), end='')
+        print("{:c}".format(ord(a) - cnv), end='')
     print()
