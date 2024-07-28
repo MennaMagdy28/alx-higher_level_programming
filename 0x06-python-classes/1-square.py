f3171db5065b22ce9d384877b6a8c46ba1571c9c
menna magdy <menna11hassanien@gmail.com>
2024-07-28 19:23:37 +0300

    moroX

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class square"""
+
+class Square:
+    """ class Square"""
+    def __init__(self, size):
+        """square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
